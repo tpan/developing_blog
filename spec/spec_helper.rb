fc0bbcc52adc31f40a143250ae3107b746bd7b12
tpan <thomastpan@gmail.com>
2022-03-22 20:30:37 -0700

    Add test environment codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'simplecov'
 SimpleCov.start
 
+if ENV['test'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
