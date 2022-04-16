b35c182cf8493cacd434dc0bd683bf95db528810
tpan <thomastpan@gmail.com>
2022-04-15 19:27:07 -0700

    Remove generated specs for now

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
-  describe "GET /index" do
+  describe "GET article index" do
     it "returns http success" do
-      get "/articles/index"
+      get "/articles#index"
       expect(response).to have_http_status(:success)
     end
   end
