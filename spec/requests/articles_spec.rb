b48de85fda0da319a461fabafb7c01679b90a830
tpan <thomastpan@gmail.com>
2022-05-03 21:57:11 -0700

    CR feedback, wip test for non existent resource update

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
-
-  before(:all) do
-    FactoryBot.create(:article)
-  end
-
   describe "GET article index" do
+    let(:article) { build(:article, title: Faker::GreekPhilosophers.name,
+        body: Faker::GreekPhilosophers.quote )}
+
     it "returns success with the articles index template" do
       get articles_path
 
       expect(response).to have_rendered(:index)
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe "GET /articles/:id" do
-    it "returns success with the articles index template" do
-      get article_path(1)
+    
+    let(:article) { create(:article, title: Faker::GreekPhilosophers.name,
+      body: Faker::GreekPhilosophers.quote )}
+
+    it "returns success with the article/:id show template" do
+      get article_path(article)
 
       expect(response).to have_rendered(:show)
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
   end
 
@@ -36,16 +38,18 @@ RSpec.describe "Articles", type: :request do
   describe "POST /articles" do
     context "with valid parameters" do
       it "successfully creates an article" do
-        post articles_path, :params => {:article => {:title => Faker::Hipster.sentence(word_count: 3), :body => Faker::Hipster.paragraph}}
+        post articles_path, params: {article: {title: Faker::Hipster.sentence(word_count: 3), body: Faker::Hipster.paragraph}}
 
+        expect(response).to redirect_to(article_path(Article.last))
         expect(response).to have_http_status(:found)
       end
     end
 
     context "with invalid parameters" do
       it "responds with unprocessable entity" do
-        post articles_path, :params => {:article => {:title => "", :body => "Foo"}}
+        post articles_path, params: {article: {title: "", body: "Foo"}}
 
+        expect(response).to have_rendered(:new)
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
@@ -53,22 +57,40 @@ RSpec.describe "Articles", type: :request do
 
   describe "PUT /articles/:id" do
     context "with valid parameters" do
-      it "successfully updates the article" do 
-        put article_path(1), :params => {:article => {:title => Faker::Hipster.sentence(word_count:3), :body => Faker::Hipster.paragraph}}
-        expect(response).to have_http_status(:found)
+      let(:article) { create(:article, title: Faker::GreekPhilosophers.name,
+        body: Faker::GreekPhilosophers.quote )}
+
+      context "when the article exists" do
+        it "successfully updates the article" do 
+          put article_path(article), params: {article: {title: Faker::Hipster.sentence(word_count:3), body: Faker::Hipster.paragraph}}
+          expect(response).to have_http_status(:found)
+        end
+      end
+
+      context "when the article does not exist" do
+        it "fails to update the article" do 
+          expect{
+            put article_path(420), params: {article: {title: Faker::Hipster.sentence(word_count:3), body: Faker::Hipster.paragraph}}
+          }.to raise_error(ActionController::RoutingError)
+        end
       end
     end
 
     context "with invalid parameters" do
-      it "throws an exception and responds with unprocessable entity" do 
-        put article_path(1), :params => {:article => { :title => nil, :body => nil }}
+      let(:article) { create(:article, title: Faker::GreekPhilosophers.name,
+        body: Faker::GreekPhilosophers.quote )}
 
+      it "throws an exception and responds with unprocessable entity" do 
+        put article_path(article), params: {article: {title: nil, body: nil} }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
   describe "DELETE /articles/:id" do
+    let(:article) { create(:article, title: Faker::GreekPhilosophers.name,
+      body: Faker::GreekPhilosophers.quote )}
+
     it "reduces articles count by 1" do
       expect{delete article_path(1)}.to change{Article.count}.by(-1)
 
