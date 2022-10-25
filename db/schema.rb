240382c83b8f8f26e3d29a1aae412f0ed8d6767b
Thomas Pan <thomastpan@gmail.com>
2022-10-24 17:03:00 -0700

    Week3/pretty urls (#10)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_013814) do
+ActiveRecord::Schema.define(version: 2022_05_11_041231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2022_04_07_013814) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
 
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :article do
         title { Faker::GreekPhilosophers.name }
         body { Faker::GreekPhilosophers.quote }
-
+        slug { Faker::GreekPhilosophers.name}
         trait :no_body do
             body { nil }
         end
