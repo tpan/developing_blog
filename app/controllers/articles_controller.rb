54c5c6525d2269e8f60127af2c53be47ddcb3250
tpan2 <tpan@scopesecurity.com>
2022-11-15 17:43:23 -0800

    Added links to jump to each page directly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,14 @@
 class ArticlesController < ApplicationController
-  ARTICLES_PER_PAGE = 5
   def index
     # Number of articles to show
     # How many articles per page
     # Ordering of articles to display
+    @articles_per_page = params.fetch(:articles_per_page, 5).to_i
 
     @page = params.fetch(:page, 0).to_i
-    @articles = Article.offset(@page * ARTICLES_PER_PAGE).limit(ARTICLES_PER_PAGE)
-    @last_page = Article.all.size / ARTICLES_PER_PAGE
+    @pages = (0..(Article.count / @articles_per_page)).to_a
+    @articles = Article.offset(@page * @articles_per_page).limit(@articles_per_page)
+    @last_page = Article.count / @articles_per_page
   end
 
   def show
@@ -51,6 +52,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :articles_per_page)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(25).each do |n|
+    FactoryBot.create(:article)
+end
